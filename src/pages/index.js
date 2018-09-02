c0e6479501138ac12e5a3ae2d6efa9286e67548c
Joyce Lai <lai.joyce@gmail.com>
2018-09-02 16:23:50 -0700

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi there!</h1>
+    <p>This is a fun Gatsby site.</p>
+    <p>Wow. This is great! </p>
+    <Link to="/page-2/">Go to the next page!</Link>
+  </div>
+)
+
+export default IndexPage
