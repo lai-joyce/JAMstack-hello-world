2cf31b70d955fffb4c686e8da8d00dd4f084cd8b
Joyce Lai <lai.joyce@gmail.com>
2018-09-23 18:41:08 -0700

    index.js title update

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,8 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hi there!</h1>
-    <p>This is a fun Gatsby site.</p>
-    <p>Wow. This is great! </p>
+    <p>This is a fun Gatsby site! </p>
+    <p>Wow. This is the starter for IBM Call For Code </p>
     <Link to="/page-2/">Go to the next page!</Link>
   </div>
 )
