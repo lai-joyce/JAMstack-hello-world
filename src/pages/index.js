fc89b7b47233b5ba3ac544613e561e3298a5ebc8
Joyce Lai <lai.joyce@gmail.com>
2023-09-28 18:27:04 -0700

    5 years later

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi there!</h1>
+    <h1>Hi there!  It's been 5 years!</h1>
     <p>This is a fun Gatsby site! </p>
     <p>This is the starter for IBM Call For Code </p>
     <Link to="/page-2/">Go to the next page!</Link>
