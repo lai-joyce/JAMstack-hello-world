cbbb91aa1a1fff200735dc2b4e3705c29a5b17c5
Joyce Lai <lai.joyce@gmail.com>
2023-09-28 18:28:54 -0700

    more words to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const IndexPage = () => (
   <div>
     <h1>Hi there!  It's been 5 years!</h1>
-    <p>This is a fun Gatsby site! </p>
+    <p>This is a fun Gatsby site! Wow, crazy stuff!</p>
     <p>This is the starter for IBM Call For Code </p>
     <Link to="/page-2/">Go to the next page!</Link>
   </div>
